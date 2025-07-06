3ea92af2df2e6c0fc234d55975269e554c151ba1
liliana <liliana.amodeo@studenti.unitn.it>
2025-07-06 16:14:06 +0200

    add vuetify

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import 'bootstrap'
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const vuetify = createVuetify()
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
