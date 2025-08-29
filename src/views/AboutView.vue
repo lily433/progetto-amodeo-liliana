e6e48fae46e37f50dd481e5f40da2e975970cda6
liliana <liliana.amodeo@studenti.unitn.it>
2025-08-29 21:27:29 +0200

    refactor: minor

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,13 +2,15 @@
 
 <template>
   <div class="container-sm-fluid h-100 d-flex justify-content-center align-items-center m-2">
-    <div class="text-center p-sm-3" style="max-width: 280px">
+    <div class="text-center p-sm-3 w-100 d-flex flex-column align-items-center">
       <img
         src="@/assets/profile-pic.png"
         alt="Profile picture"
         class="img-fluid rounded-circle shadow mx-auto d-block mb-3"
+        style="max-width: 300px"
       />
       <h2 class="display-4 display-md-3 display-xl-2">Liliana Amodeo</h2>
+      <p class="lead fs-7 fs-md-6 fs-lg-5 fw-bold">liliana.amodeo@studenti.unitn.it</p>
       <p class="lead fs-7 fs-md-6 fs-lg-5 fw-bold">237244</p>
     </div>
   </div>
